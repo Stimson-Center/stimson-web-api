9dcb77fd2b4b7faaa7f643622d3829c8dc9653ad
ascooper57 <cooper@pobox.com>
2020-07-18 19:01:58 -0700

    extended timeout, renamed instance

--- run_deploy.sh
+++ run_deploy.sh
@@ -4,9 +4,9 @@ set -e
 gcloud -v
 gcloud components install app-engine-python
 gcloud components update
-gcloud config set project stimson-web-curator-api
-# gcloud app deploy --project=stimson-web-curator-api --stop-previous-version --promote --verbosity=debug
-gcloud app deploy --project=stimson-web-curator-api --promote
-gcloud app describe --project=stimson-web-curator-api
+gcloud config set project stimson-web-api
+# gcloud app deploy --project=stimson-web-api --stop-previous-version --promote --verbosity=debug
+gcloud app deploy --project=stimson-web-api --promote
+gcloud app describe --project=stimson-web-api
 gcloud app browse
 gcloud app logs tail -s default
