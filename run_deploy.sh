ffe310a852c59a55d00cf2f47ef98599171e432f
ascooper57 <cooper@pobox.com>
2020-07-19 12:33:51 -0700

    Insane Google App Engine build / deploy timeout workaroud
    https://stackoverflow.com/questions/60431732/gcp-cloud-build-ignores-timeout-settings

--- run_deploy.sh
+++ run_deploy.sh
@@ -5,8 +5,9 @@ gcloud -v
 gcloud components install app-engine-python
 gcloud components update
 gcloud config set project stimson-web-api
+gcloud config set app/cloud_build_timeout 1400
 # gcloud app deploy --project=stimson-web-api --stop-previous-version --promote --verbosity=debug
-gcloud app deploy --project=stimson-web-api --promote
+gcloud app deploy --project=stimson-web-api --promote --quiet
 gcloud app describe --project=stimson-web-api
 gcloud app browse
 gcloud app logs tail -s default
