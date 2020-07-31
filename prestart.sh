d109772773007115dcd700fc95d91496b0b50437
ascooper57 <cooper@pobox.com>
2020-07-30 17:20:54 -0700

    run_deploy did not like this env variable

--- prestart.sh
+++ prestart.sh
@@ -2,7 +2,6 @@
 
 echo "Running customized /app/prestart.sh:"
 export GOOGLE_APPLICATION_CREDENTIALS=/app/.GOOGLE_APPLICATION_CREDENTIALS.json
-export GCS_STORAGE_BUCKET: "stimson-gcs-datascience-bucket"
 export NGINX_HTTP_PORT_NUMBER=8080
 # export NGINX_HTTPS_PORT_NUMBER=8443
 
