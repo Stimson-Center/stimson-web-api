98ada0575ed67ff0868377b8aeaa89ade9345d14
ascooper57 <cooper@pobox.com>
2020-07-31 20:32:43 -0700

    Rin system test and updated google drive

--- tests/system/stimson_urls_test.py
+++ tests/system/stimson_urls_test.py
@@ -1,40 +1,11 @@
 # -*- coding: utf-8 -*-
 
-import json
 import os
 from time import time
 
 from scraper import Article, Configuration
-from scraper.urls import get_path
+
 from ..mthreading import NewsPool
-
-
-def save_article(article, filename, filedir='/tmp'):
-    if article.text.strip() == '':
-        print(f"Error: Empty Article:{article.url}")
-    else:
-        a = {
-            'title': article.title,
-            'authors': article.authors,
-            'publish_date': article.publish_date,
-            'summary': article.summary,
-            'keywords': article.keywords,
-            'text': article.text,
-            'url': article.url,
-            'tables': article.tables,
-            'language': article.meta_lang,
-            'html': article.html
-        }
-        try:
-            filepath = os.path.join(filedir, filename)
-            # noinspection PyUnusedLocal
-            with open(filepath, "w", encoding='utf-8') as f:
-                f.write(json.dumps(a, indent=4, sort_keys=True))
-                # f.write(a['title'] + '\n')
-                # f.write(a['text'] + '\n')
-        except OSError as ex:
-            # includes IOError
-            pass
 
 
 def article_thread_pool(urls, config):
@@ -45,7 +16,7 @@ def article_thread_pool(urls, config):
     return articles
 
 
-def article_curator(test_driver_file, config):
+def article_curator(client, test_driver_file, config):
     print("\n")
     # set variables
     start_time = time()
@@ -58,7 +29,6 @@ def article_curator(test_driver_file, config):
             config.set_language(parts[0])
             urls.append(parts[1].replace('\n', '').strip())
 
-    filedir = os.getenv('HOME')
     # spin up
     articles = article_thread_pool(urls, config)
     bad_titles = [
@@ -74,54 +44,47 @@ def article_curator(test_driver_file, config):
         "page not found - the australian-thai chamber of commerce (austcham)"
     ]
     for article in articles:
-        try:
-            title = article.title if article.title else ''
-            title = title.replace("/", " ").replace("\\", " ").strip()
-            if title == '':
-                title = get_path(article.url)
-                title = title.replace("/", "-")
-            elif title.lower() in bad_titles:
-                print(f"Error: {title} {article.url}")
-                continue
-            publish_date = article.publish_date[0:10].strip() if article.publish_date else ''
-            article.publish_date = publish_date
-            if publish_date:
-                filename = f'{publish_date}_{title}.json'
-            else:
-                filename = f'{title}.json'
-            # encoded = b64_encode(article.url)
-            # filename = encoded + '.json'
-            # # filename = article.link_hash + '.json'
-            save_article(article, filename, filedir=filedir)
-        except Exception as exc:
-            print('%r generated an exception: %s' % (article.url, exc))
+        if article.text.strip() == '':
+            print(f"Error: Empty Article:{article.url}")
+        else:
+            try:
+                response = client.post("/pdf", json=article.get_json())
+                assert 200 == response.status_code
+                assert '200 OK' == response.status
+                assert 'utf-8' == response.charset
+                assert response.data
+            except Exception as ex:
+                print('%r generated an exception: %s' % (article.url, ex))
 
     total_elapsed_time = time() - start_time
     print(f'Total elapsed time {total_elapsed_time} seconds')
 
 
 # noinspection PyUnresolvedReferences
-def test_industrial_spaces_urls(fixture_directory):
+@pytest.mark.skip(reason="Running this test updates Google Drive")
+def test_industrial_spaces_urls(fixture_directory, client):
     config = Configuration()
     config.follow_meta_refresh = True
     test_driver_file = os.path.join(fixture_directory, "url", "industrial-spaces-urls.csv")
-    article_curator(test_driver_file, config)
+    article_curator(client, test_driver_file, config)
 
 
 # noinspection PyUnresolvedReferences
-def test_illegal_unreported_and_unregulated_fishing_urls(fixture_directory):
+@pytest.mark.skip(reason="Running this test updates Google Drive")
+def test_illegal_unreported_and_unregulated_fishing_urls(fixture_directory, client):
     config = Configuration()
     config.follow_meta_refresh = True
     config.use_meta_language = False
     config.language = 'en'
     config.http_success_only = False
     test_driver_file = os.path.join(fixture_directory, "url", "illegal-unreported-and-unregulated-fishing-urls.csv")
-    article_curator(test_driver_file, config)
+    article_curator(client, test_driver_file, config)
 
 
 # noinspection PyUnresolvedReferences
-def test_energy_investment_mekong_delta_urls(fixture_directory):
+@pytest.mark.skip(reason="Running this test updates Google Drive")
+def test_energy_investment_mekong_delta_urls(fixture_directory, client):
     config = Configuration()
     config.follow_meta_refresh = True
     test_driver_file = os.path.join(fixture_directory, "url", "energy-investment-mekong-delta-Thailand-urls.csv")
-    article_curator(test_driver_file, config)
+    article_curator(client, test_driver_file, config)
