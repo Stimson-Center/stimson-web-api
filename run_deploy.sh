de37e6a876faf24f3f8282bdbc8620e97c6ed998
ascooper57 <cooper@pobox.com>
2021-12-18 12:26:40 -0800

    Update run_deploy.sh

--- run_deploy.sh
+++ run_deploy.sh
@@ -4,6 +4,7 @@ set -e
 gcloud -v
 gcloud components install app-engine-python
 gcloud components update
+gcloud config set account `hstimson89@gmail.com`
 gcloud config set project stimson-web-api
 gcloud config set app/cloud_build_timeout 1400
 # gcloud app deploy --project=stimson-web-api --stop-previous-version --promote --verbosity=debug
